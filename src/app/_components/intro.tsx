7f2772068ecfcdba257f586ce2a1005a3600210c
Carlos Eduardo Rodríguez Torres <cuentamayorc4@gmail.com>
2025-07-28 01:42:37 -0600

    cambios en el titulo

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,11 +1,20 @@
-import { CMS_NAME } from "@/lib/constants";
+import Image from "next/image";
 
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        JRMC Real State
-      </h1>
+      <div className="flex items-center">
+        <Image
+          src="/assets/blog/intro/Logo.png" // Cambia esta ruta por la de tu imagen
+          alt="JRMC Real State Logo"
+          width={200} // Ajusta el tamaño según necesites
+          height={200}
+          className="mr-1" // Espacio entre la imagen y el texto
+        />
+        <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+          JRMC Real State
+        </h1>
+      </div>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
          Compartimos nuestra experiencia en el mundo inmobiliario.
       </h4>
